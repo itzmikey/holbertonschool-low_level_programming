df4dea328aa6529c15915c47643bba3467c0e21e
Michael Li <itzmiku@gmail.com>
2018-06-21 21:36:09 +0000

    Define n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,14 +16,14 @@ int main(void)
 	}
 	else
 	{
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	else
+	if (n =< 0)
 	{
 		printf("%d is zero\n", n);
 	}
+	else
+	{
+		printf("%d is negative\n", n);
 	}
-	return 0;
+	}
+	return (0);
 }
