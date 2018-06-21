35f18103d137f036e5cfda1454ae1b6cf6bc47a1
Michael Li <itzmiku@gmail.com>
2018-06-21 23:31:19 +0000

    Define n from the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	star = n % 10;
 
-	printf("Last digit of %d is ", n, star);
+	printf("Last digit of %d is ", n);
 	if (star > 5)
 		printf("%d and is greater than 5\n", star);
 	else if (star == 0)
