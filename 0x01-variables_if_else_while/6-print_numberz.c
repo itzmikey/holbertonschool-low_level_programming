e5c298afc1897e7f1b8c3cd1fd9006db55123dcb
Michael Li <itzmiku@gmail.com>
2018-06-22 01:11:31 +0000

    Put 0-9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-	int n;
+	int n = 0;
 
-	for (n = 0; n < 10; n++)
+	while (n < 10)
 	{
-		putchar("%d", n);
+		putchar(n + 0);
+		n++;
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
