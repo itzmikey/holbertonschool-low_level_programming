ea90bbfbce6d3307c6fa35eafef15d414a3f6b06
Michael Li <itzmiku@gmail.com>
2018-06-22 00:31:12 +0000

    List the alphabet take out the q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char Word;
+	char Word;
 
-        for (Word = 'a'; Word <= 'z'; Word++)
-		if (Word != 'q'|| Word != 'e')
-                putchar(Word);
-        putchar('\n');
-        return (0);
+	for (Word = 'a'; Word <= 'z'; Word++)
+	{
+	if (Word != 'q' && Word != 'e')
+	{
+		putchar(Word);
+	}
+	putchar('\n');
+	}
+	return (0);
 }
