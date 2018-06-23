ab46d38ab804e14b7567a38eb92470eeee072b72
Michael Li <itzmiku@gmail.com>
2018-06-23 00:47:00 +0000

    print all different combo 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,17 +11,17 @@ int main(void)
 	int i;
 	int m;
 
-        for (n = 0; n < 8; n++)
-                for (i = n + 1; i < 9; i++)
+	for (n = 0; n < 8; n++)
+		for (i = n + 1; i < 9; i++)
 			for (m = i + 1; m < 10; m++)
 			{
 				putchar(n + 48);
 				putchar(i + 48);
 				putchar(m + 48);
-			if (n == '7' && i == '8' && m == '9')
-				break;
-			putchar(',');
-			putchar(' ');
+				if (n == 7 && i == 8 && m == 9)
+					break;
+				putchar(',');
+				putchar(' ');
 			}
 	putchar('\n');
 	return (0);
