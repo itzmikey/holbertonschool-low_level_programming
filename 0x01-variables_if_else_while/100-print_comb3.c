df23eb152227587d5a1a4acadfb1f11079e02f87
Michael Li <itzmiku@gmail.com>
2018-06-23 00:09:25 +0000

    Print all different combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - PID
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+	int i;
+
+	for (n = 0; n < 9; n++)
+		for (i = n+1; i < 10; i++)
+		{
+			putchar(n +48);
+			putchar(i +48);
+			if (n == '8' && i == '9')
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
