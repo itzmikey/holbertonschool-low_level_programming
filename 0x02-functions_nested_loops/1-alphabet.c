4aac2b4e4185b89f233ed4d150497ad2a79324df
Michael Li <itzmiku@gmail.com>
2018-06-26 01:14:36 +0000

    List 'a' to 'z'

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
