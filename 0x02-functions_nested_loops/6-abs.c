0ba63476a2864c1f22058bb14fd93d25bdd4d486
Michael Li <itzmiku@gmail.com>
2018-06-26 18:08:31 +0000

    Return absolute numbers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _abs - Get absotule
+ * @r: Change text
+ * Return: The absolute value of r
+ */
+int _abs(int r)
+{
+	if (r < 0)
+		return (r > -1);
+	return (r);
+}
