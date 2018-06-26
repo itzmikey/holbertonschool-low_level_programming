b0f70dc9c9f0725324b74d303d376aa81a75d6d4
Michael Li <itzmiku@gmail.com>
2018-06-26 21:43:53 +0000

    Print the last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,16 +3,16 @@
 /**
  * print_last_digit - Prints the last digit
  * @r: Change text
- * Return: Always 0.
+ * Return: Value of the last digit.
  */
 int print_last_digit(int r)
 {
-	char m;
+	int m;
 
 	if (r >= 0)
 		m = r % 10;
 	else
-		m = -r % 10;
-	_putchar(m + '0');
+		m = -(r % 10);
+	_putchar('0' + m);
 	return (m);
 }
