871ffad74552623d37b46d6505d605e898ad1286
Michael Li <itzmiku@gmail.com>
2018-06-27 00:03:34 +0000

    Combine 2 integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * add - Add 2 integers and get the value
+ * @a: Value 1
+ * @b: Value 2
+ * Return: Add both integers.
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
