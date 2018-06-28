886fd78677da2a6722c0d6d25178a7700cab34d5
Michael Li <itzmiku@gmail.com>
2018-06-28 03:05:52 +0000

    Multiple of 3 and 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,15 +4,15 @@
  * main - List out all multiples of 3 and 5
  * Return: Always 0
  */
+
 int main(void)
 {
 	int a;
 	int x;
 
-	x = 0;
 	for (a = 0; a < 1024; a++)
 		if (a % 3 == 0 || a % 5 == 0)
 			x += a;
-	printf("%d\n" x);
+	printf("%d\n", x);
 	return (0);
 }
