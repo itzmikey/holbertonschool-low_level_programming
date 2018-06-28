50da1d16efd34cff60b4bf37986dfe5b4cf73fd1
Michael Li <itzmiku@gmail.com>
2018-06-28 22:45:04 +0000

    Prints 10 times the numbers

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - Prints 10 times the numbers, from 0 - 14
+ */
+void more_numbers(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c < 15; c++)
+		{
+			if (c > 9)
+			{
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
