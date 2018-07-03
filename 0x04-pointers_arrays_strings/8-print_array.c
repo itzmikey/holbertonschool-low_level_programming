09ea4900a6b85c914ac6c5d5759f719594733a02
Michael Li <itzmiku@gmail.com>
2018-07-03 19:28:56 +0000

    Print array

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - Define n into array of integers.
+ * @a: Arrays
+ * @n: Print amount
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf ("%d", a[i]);
+	if (i != n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
