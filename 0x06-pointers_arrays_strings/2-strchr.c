c8093b14ed6dfec543c6c2d7537a3d8ba9338e6b
Michael Li <itzmiku@gmail.com>
2018-07-10 19:26:48 +0000

    Locate string charater

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strchr - Locates a charater from a string
+ * @s: String of charaters
+ * @c: Character to look for
+ * Return: NULL if no letter repeats else return the first repeat charater.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for ( i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
+	return (NULL);
+}
