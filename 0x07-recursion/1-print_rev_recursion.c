06f46bc2c10651eef0b3567591d5fb79482973d5
Michael Li <itzmiku@gmail.com>
2018-07-11 17:58:21 +0000

    Reverse the string

---
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - Print string in reverse
+ * @s: string to be reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == 0)
+	{
+		return;
+	}
+	else
+	{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+	}
+}
