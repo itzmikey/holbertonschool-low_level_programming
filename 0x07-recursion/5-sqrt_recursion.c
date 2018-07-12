a5593414a296bf956f286266267cec50ff8f172f
Michael Li <itzmiku@gmail.com>
2018-07-12 02:10:36 +0000

    Find squareroot number

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_function - to get sqaureroot program
+ * @n: Squareroot number
+ * @i: Integers
+ * Return: Squareroot number.
+ */
+int sqrt_function(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	return (sqrt_function(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - return the natural sqaureroot of a number
+ * @n: Number of squareroot
+ * Return: Squareroot number
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 0;
+
+        return (sqrt_function(n, i));
+}
